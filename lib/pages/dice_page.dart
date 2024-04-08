7981f43a21967de5ed316923fa8341f5681e5029
Abdul Fatah <fatah03111995@gmail.com>
2024-04-09 02:54:46 +0700

    feat : adding dice image, adding some styling

--- lib/pages/dice_page.dart
+++ lib/pages/dice_page.dart
@@ -10,6 +10,23 @@ class DicePage extends StatefulWidget {
 class _DicePage extends State<DicePage> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return Scaffold(
+      backgroundColor: Colors.red[800],
+      appBar: AppBar(
+        backgroundColor: Colors.red[900],
+        title: const Text(
+          'Dice Simple App',
+          style: TextStyle(fontFamily: 'Poppins', color: Colors.white),
+        ),
+      ),
+      body: Center(
+        child: Row(
+          children: [
+            Expanded(child: Image.asset('assets/images/dice1.png')),
+            Expanded(child: Image.asset('assets/images/dice1.png'))
+          ],
+        ),
+      ),
+    );
   }
 }
